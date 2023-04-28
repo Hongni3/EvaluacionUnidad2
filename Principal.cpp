407d70eddf6e665de42f1b1d0af50d3c8aa955e2
Ingenieria Sistemas31 <isi31@lab.uca.edu.ni>
2023-04-28 07:48:59 -0600

    Primercommit

---
+++ Principal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#define MAX 100
+
+int main(int argc, char const *argv[]){
+
+    int materia, nota;
+    cout<< "Ingrese la materia:"<<endl;
+    cin>> materia;
+    cout<< "Ingrese la nota: " <<endl;
+    cin>> nota;
+    
+
+    return 0;
+}
